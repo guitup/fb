f3921bc837ab78b41fce93175a44c5db89b10e4b
guitup <56364208+guitup@users.noreply.github.com>
2019-10-24 20:12:06 +0200

    Update config.php

--- config.php
+++ config.php
@@ -6,8 +6,8 @@ $db_user = 'root';
 $db_pass = '';
 $db_host = 'localhost';
 $connected = '';
-$appid = '177898755600101';
-$appsecret = 'b36cb353645e0d38aad95af9553fc402';
+$appid = 'xxx';
+$appsecret = 'xxx';
 $apiversion = 'v4.0';
 require_once __DIR__ . '/vendor/autoload.php'; // change path as needed
 ?>
